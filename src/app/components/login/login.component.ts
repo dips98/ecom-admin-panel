dcaf2518ddf7a3b457b38b58a8ee231cd4b4351f
dips98 <upadhayay.dipesh@gmail.com>
2024-04-21 21:54:28 +0530

    Login page validation form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!:FormGroup 
+
+  ngOnInit():void{
+    this.setForm();
+  }
+
+  setForm(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,Validators.maxLength(5)])
+    })
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      console.log('Login Successful ',this.loginForm.value);
+      alert('Login Successfull!')
+    }
+  }
 
 }
