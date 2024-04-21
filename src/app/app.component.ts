50ad46c5b0d73e9984727cbae6b8a3f6f5a3b93d
dips98 <upadhayay.dipesh@gmail.com>
2024-04-21 21:54:52 +0530

    Dependency for component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet,
+    CommonModule,
+    LoginComponent,
+    RegisterComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    DashboardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
