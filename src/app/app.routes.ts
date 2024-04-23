6cf66086a4dac4590743fe1dc26996fcc5f4be54
dips98 <upadhayay.dipesh@gmail.com>
2024-04-23 23:50:31 +0530

    Added routing for child dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { SalesComponent } from './components/sales/sales.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
@@ -12,11 +17,11 @@ export const routes: Routes = [
         component: DashboardComponent,
         children: [
             { path: '', component: HomeComponent },
-            { path: 'products', component: HomeComponent },
-            { path: 'orders', component: HomeComponent },
-            { path: 'customers', component: HomeComponent },
-            { path: 'inventry', component: HomeComponent },
-            { path: 'sales', component: HomeComponent }
+            { path: 'products', component: ProductsComponent },
+            { path: 'orders', component: OrdersComponent },
+            { path: 'customers', component: CustomersComponent },
+            { path: 'inventory', component: InventoryComponent },
+            { path: 'sales', component: SalesComponent }
         ]
 
     }
