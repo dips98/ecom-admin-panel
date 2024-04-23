fc0779050009a28a45bcba0f9346861b09228aec
dips98 <upadhayay.dipesh@gmail.com>
2024-04-23 23:25:16 +0530

    adding chid dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path:'register', component:RegisterComponent},
     {path:'', component:LoginComponent},
-    {path:'dashboard', component: DashboardComponent }
+    {path:'dashboard',
+     component: DashboardComponent,
+     children: [{path:'',component: HomeComponent}]
+
+    }
 ];
