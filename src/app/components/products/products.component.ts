b349c82e0a0c114292076f5800c7abff692224fe
dips98 <upadhayay.dipesh@gmail.com>
2024-04-25 03:00:23 +0530

    Added

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+
+  productForm! :FormGroup;
+  productList:any = [];
+
+  constructor(
+     
+  ){
+
+  }
+
+  ngOnInit():void{
+    this.setForm();
+  }
+
+  setForm(){
+    this.productForm = new FormGroup({
+      productId: new FormControl(''),
+      productName: new FormControl(''),
+      productRate: new FormControl(''),
+      productQuantity: new FormControl('')
+    })
+  }
+
+  submit(){
+    this.productList.push(this.productForm.value);
+    console.log(this.productList);
+    this.productForm.reset();
+  }
 
 }
