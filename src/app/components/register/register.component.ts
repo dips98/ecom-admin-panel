4fa0aba876c552942cc8d3485ee45f0210d822ff
dips98 <upadhayay.dipesh@gmail.com>
2024-05-01 17:02:14 +0530

    Register control

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  userRegForm!: FormGroup;
+
+  ngOnInit():void{
+    this.setForm();
+  }
+  
+  setForm(){
+
+  }
 }
