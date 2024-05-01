bf886b0f6edc16919b81821828362372f44d0a77
dips98 <upadhayay.dipesh@gmail.com>
2024-05-01 22:30:05 +0530

    Register flow done

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,44 +1,49 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ModelService } from '../../services/model.service';
+import {  NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule,RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink,NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
   constructor(
-    private modelService:ModelService
-  ){}
+    private modelService: ModelService
+  ) { }
 
   userRegForm!: FormGroup;
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.setForm();
   }
 
-  setForm(){
+  setForm() {
     this.userRegForm = new FormGroup({
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      contact: new FormControl(''),
-      email: new FormControl(''),
-      password: new FormControl(''),
-      confirmPassword: new FormControl(''),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      contact: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(5)]),
     });
   }
 
-  async doRegister(){
-    const body = this.userRegForm.value;
-    (await this.modelService.resgisterUser(body)).subscribe((data:any)=>{
-      console.log(data);
-      this.userRegForm.reset();
-      alert(data.msg);
-    })
+  async doRegister() {
+    if (this.userRegForm.valid) {
+      const body = this.userRegForm.value;
+      (await this.modelService.resgisterUser(body)).subscribe((data: any) => {
+        console.log(data);
+        this.userRegForm.reset();
+        alert(data.msg);
+      })
+    } else {
+      alert("Please fill valid form");
+    }
   }
 }
