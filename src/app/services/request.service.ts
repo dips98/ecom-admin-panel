afc156008e93b359a78b8f64b8b5d28a75856195
dips98 <upadhayay.dipesh@gmail.com>
2024-05-03 01:56:59 +0530

    Req s using

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UrlConfigService } from './url-config.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,23 +8,24 @@ import { Injectable } from '@angular/core';
 
 export class RequestService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private configUrl: UrlConfigService
   ) { }
 
   registerUser(body: any) {
-    var url = 'http://localhost:3000/register'
+    var url = `${this.configUrl.urlhost}/register`
     const Res = this.http.post(url, body);
     return Res;
   }
 
   loginUser(body: any) {
-    var url = 'http://localhost:3000/login'
+    var url = `${this.configUrl.urlhost}/login`
     const Res = this.http.post(url, body);
     return Res;
   }
 
   addProduct(body: any) {
-    var url = 'http://localhost:3000/addProduct'
+    var url = `${this.configUrl.urlhost}/addProduct`
     const Res = this.http.post(url, body);
     return Res;
   }
