79ba7d42d8198590f14c16f96e4d5048c753b464
dips98 <upadhayay.dipesh@gmail.com>
2024-05-05 00:00:15 +0530

    added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../components/navigation/navigation.component';
 
 @Component({
-  
   selector: 'app-dashboard',
   standalone: true,
   imports: [RouterOutlet, NavigationComponent],
